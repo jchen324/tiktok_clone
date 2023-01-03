4cb215487ea64fcb046a6e9acfd8bf7bb70e463e
jchen324 <jchen324@jh.edu>
2023-01-03 17:55:34 -0500

    Fix type error in Navbar and improve UI

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/legacy/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -6,6 +6,7 @@ import { AiOutlineLogout } from "react-icons/ai";
 import { BiSearch } from "react-icons/bi";
 import { IoMdAdd } from "react-icons/io";
 import { GoogleLogin, googleLogout } from "@react-oauth/google";
+import { IUser } from "../types";
 
 import Logo from "../utils/tiktik-logo.png";
 import { createOrGetUser } from "../utils";
@@ -13,7 +14,12 @@ import { createOrGetUser } from "../utils";
 import useAuthStore from "../store/authStore";
 
 const Navbar = () => {
+  const [user, setUser] = useState<IUser | null>();
   const { userProfile, addUser, removeUser } = useAuthStore();
+  useEffect(() => {
+    setUser(userProfile);
+  }, [userProfile]);
+
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
@@ -29,20 +35,20 @@ const Navbar = () => {
 
       <div>SEARCH</div>
       <div>
-        {userProfile ? (
+        {user ? (
           <div className="flex gap-5 md:gap-10">
             <Link href="/upload">
-              <button className="border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2">
+              <button className="border-2 p-2 md:px-4 text-md font-semibold flex items-center gap-2">
                 <IoMdAdd className="text-xl" />{" "}
                 <span className="hidden md:block">Upload </span>
               </button>
             </Link>
-            {userProfile.image && (
+            {user.image && (
               <Link href="/">
                 <div>
                   <Image
                     className="rounded-full cursor-pointer"
-                    src={userProfile.image}
+                    src={user.image}
                     alt="user"
                     width={40}
                     height={40}
