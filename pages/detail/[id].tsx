9b8e60f594ad0656f254cc1f64c2a2dd1b7c9749
jchen324 <jchen324@jh.edu>
2023-01-05 13:51:53 -0500

    Complete the left part of video detail page

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,9 +1,100 @@
-import React from 'react'
+import { useEffect, useRef, useState } from "react";
+import { BsFillPlayFill } from "react-icons/bs";
+import { MdOutlineCancel } from "react-icons/md";
+import { HiVolumeUp, HiVolumeOff } from "react-icons/hi";
 
-const Detail = () => {
-  return (
-    <div>Detail</div>
-  )
+import axios from "axios";
+import { Video } from "../../types";
+import { BASE_URL } from "../../utils";
+import { useRouter } from "next/router";
+
+interface IProps {
+  postDetails: Video;
 }
 
-export default Detail
+const Detail = ({ postDetails }: IProps) => {
+  const [post, setPost] = useState(postDetails);
+  const [Playing, setPlaying] = useState<boolean>(false);
+  const [isVideoMuted, setIsVideoMuted] = useState<boolean>(false);
+  const router = useRouter();
+
+  const onVideoClick = () => {
+    if (Playing) {
+      videoRef?.current?.pause();
+      setPlaying(false);
+    } else {
+      videoRef?.current?.play();
+      setPlaying(true);
+    }
+  };
+
+  useEffect(() => {
+    if (post && videoRef?.current) {
+      videoRef.current.muted = isVideoMuted;
+    }
+  }, [post, isVideoMuted]);
+
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  if (!post) return null;
+
+  return (
+    <div className="flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap">
+      <div className="relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-black">
+        <div className="opacity-90 absolute top-6 left-2 lg:left-6 flex gap-6 z-50">
+          <p
+            className="cursor-pointer"
+            onClick={() => {
+              router.back();
+            }}
+          >
+            <MdOutlineCancel className="text-white text-[35px]" />
+          </p>
+        </div>
+        <div className="relative">
+          <div className="lg:h-[100vh] h-[60vh]">
+            <video
+              ref={videoRef}
+              onClick={onVideoClick}
+              loop
+              src={post?.video?.asset.url}
+              className="h-full cursor-pointer"
+            ></video>
+          </div>
+          <div className="absolute top-[45%] left-[45%] cursor-pointer">
+            {!Playing && (
+              <button onClick={onVideoClick}>
+                <BsFillPlayFill className="text-white text-6xl lg:text-8xl" />
+              </button>
+            )}
+          </div>
+        </div>
+        <div className="absolute bottom-5 lg:bottom-10 right-5 lg:right-10 cursor-pointer">
+          {isVideoMuted ? (
+            <button onClick={() => setIsVideoMuted(false)}>
+              <HiVolumeOff className="text-white text-3xl lg:text-4xl" />
+            </button>
+          ) : (
+            <button onClick={() => setIsVideoMuted(true)}>
+              <HiVolumeUp className="text-white text-3xl lg:text-4xl" />
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const getServerSideProps = async ({
+  params: { id },
+}: {
+  params: { id: string };
+}) => {
+  const { data } = await axios.get(`${BASE_URL}/api/post/${id}`);
+
+  return {
+    props: { postDetails: data },
+  };
+};
+
+export default Detail;
